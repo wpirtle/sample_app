bb83084090a1ce68044b8ccf511a7aeea10f039e
Wayne Pirtle <wpirtle@server>
2012-11-23 15:21:38 -0600

    Complete layout and routes

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+#def full_title(page_title)
+#	base_title = "Ruby on Rails Tutorial Sample App"
+#	if page_title.empty?
+#		base_title
+#	else
+#		"#{base_title} | #{page_title}"
+#	end
+#end
+
+# above replaced by the line below and the file spec/application_helper_spec.rb 
+include ApplicationHelper
